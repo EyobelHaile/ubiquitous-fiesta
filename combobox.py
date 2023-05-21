f87aa20d069a03ba364a41894e6fa6d899780193
EyobelHaile <130510421+EyobelHaile@users.noreply.github.com>
2023-05-21 00:22:59 -0700

    Update combobox.py

--- combobox.py
+++ combobox.py
@@ -9,17 +9,21 @@ def on_select(event):
     selected_item = event.widget.get()
     print("selected item:", selected_item)
 
-#
-root = tk.tk()
+#create the root object window 
+root = tk.Tk()
 root.title("Bella is sleepy")
 
 #create an array of item
 items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]
+
 #Create the combo box object, put the object in the root wwindow and populate values.
 combo_box = ttk.Combobox(root, values=items)
 
+#The blind function will tie the selected item to the on_selected function.
 combo_box.bind("<<ComboboxSelected>>", on_select)
 
+#packing object to the screen with the Geometry manager
 combo_box.pack()
 
+#Keeping the window visible
 root.mainloop()
